28464fd23556afc554a640cebb72a858aa08443a
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2024-12-13 14:06:13 +0900

    finished go to school

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
-
+import { Todo } from './Todo'
 export const TodoWrapper = () => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+			setTodos([...todos, {id: Math.floor(Math.random() * 100000000), 
+				task: todo, 
+				completed: false, 
+				isEditing: false				
+			 }])
+    }
 
     return (
     <div className='TodoWrapper'>
       <TodoForm addTodo={addTodo}/>
+			<Todo />
     </div>
   )
 }
