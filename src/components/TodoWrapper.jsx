559d224a59579104114bbe92e9acae212f6051d0
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2025-01-10 16:13:08 +0900

    last commit

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
 import { Todo } from './Todo'
+import { EditTodoForm} from './EditTodoForm'
 export const TodoWrapper = () => {
     const [todos, setTodos] = useState([]);
 
@@ -22,13 +23,25 @@ export const TodoWrapper = () => {
 			setTodos(todos.filter(todo => todo.id !== id))
 		}
 
+		const editTodo = (id) => {
+			setTodos(todos.map(todo => todo.id === id ? 
+				 {...todo, isEditing: !todo.isEditing} : todo
+			))
+		}
+
+		const editTask = (task, id) => {
+			setTodos(todos.map((todo) => todo.id === id ? 
+			{...todo, task, isEditing: !todo.isEditing} : todo));
+		};
+
     return (
     <div className='TodoWrapper'>
-			<h1>Get Things done</h1>
-      <TodoForm addTodo={addTodo}/>
-			{todos.map((todo, index) => 
-			<Todo task={todo} key={index} toggleComplete={toggleComplete}
-			deleteTodo={deleteTodo}/>)}
+		<h1>Get Things done</h1>
+		<TodoForm addTodo={addTodo}/>
+		{todos.map((todo, index) =>  todo.isEditing ? 
+		(<EditTodoForm task={todo} editTodo={editTask}/>) :
+		(<Todo task={todo} key={index} toggleComplete={toggleComplete}
+		deleteTodo={deleteTodo} editTodo={editTodo}/>))}
     </div>
   )
 }
